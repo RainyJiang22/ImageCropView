1039902a6c482fe03e603ff90d20c971ee99d68a
jiangshiyu <jiangshiyuxs@gmail.com>
2022-06-07 18:37:01 +0800

    initial project

---
+++ app/src/main/java/com/base/imagecropview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.base.imagecropview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
